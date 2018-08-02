22a76b4a4b3c51b660e20ca8b5c9571ffbc2b0c3
•─ ✫-[✭] ༺ เบบี้เอม ༻[✭]- ✫─• <39667511+TaBy2@users.noreply.github.com>
2018-08-02 18:52:11 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -30,7 +30,7 @@ class Config(object):
     APP_TYPE    = "CHROMEOS\t2.1.4\tCHROMEOS\t10.0.0"
     APP_VER     = '8.0.2'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'Nadya'
+    SYSTEM_NAME = 'Rakey'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
